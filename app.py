b468877b556eea1f3bcc050471625a436fa807e3
Rukesh Paudel <rukeshpaudel@gmail.com>
2024-02-08 16:08:48 +0545

    chatbot updated

--- app.py
+++ app.py
@@ -1,66 +1,84 @@
-import gradio as gr
-import os
+
 import time
+import os
+import gradio as gr
 
-# Chatbot demo with multimodal input (text, markdown, LaTeX, code blocks, image, audio, & video). Plus shows support for streaming text.
+from openai import OpenAI
 
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
-def print_like_dislike(x: gr.LikeData):
-    print(x.index, x.value, x.liked)
+# Initialize the client
+# Set your OpenAI API key
 
+'''file = client.files.create(
+    file=open("songs.txt", "rb"),
+    purpose='assistants'
+)'''
 
-def add_text(history, text):
-    history = history + [(text, None)]
-    return history, gr.Textbox(value="", interactive=False)
+# Step 1: Create an Assistant
+assistant = client.beta.assistants.create(
+    name="MeroHealthAI",
+    instructions="You are a highly qualified and skilled doctor who can ask all the right questions to the patient and create an engaging and interesting conversation and make patients let out all the diseases they are suffering from. Then you will create a medical report based on the symptoms. If you are 100% sure, you can also predict the disease else just report the symptoms in a formal formatted diagnosis report. Make sure to include all the vital informations by asking the patients. Ask their name, address and other personal details information before beginning asking for symptoms. Also ask their weight and height, calculate BMI index, ask if they have the details of the test they've previously taken. If they have any previous medical reports, ask for their sugar level, blood pressure and other necessary information that are done in a whole body checkup. Ask one question at a time so that the user doesn't feel overwhelmed. After completing asking the symptoms, automatically generate the symptoms in a medical report like format along with the patient's information.",
+    model="gpt-3.5-turbo",
+  #  file_ids=[file.id],
+    tools=[{"type": "retrieval"}]
+)
 
+# Step 2: Create a Thread
+thread = client.beta.threads.create()
 
-def add_file(history, file):
-    history = history + [((file.name,), None)]
-    return history
+def main(query, history):
+    # Step 3: Add a Message to a Thread
+    history=history,
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=query
+    )
 
+    # Step 4: Run the Assistant
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        instructions="The user is a humanitarian worker who is going through digital transformation"
+    )
 
-def bot(history):
-    response = "**That's cool!**"
-    history[-1][1] = ""
-    for character in response:
-        history[-1][1] += character
-        time.sleep(0.05)
-        yield history
+    while True:
+        # Wait for 5 seconds
+        time.sleep(0.5)
 
-
-with gr.Blocks() as demo:
-   with gr.Tab("Chatbot"):
-        chatbot = gr.Chatbot(
-            [],
-            elem_id="chatbot",
-            bubble_full_width=False,
-            avatar_images=(None, "utils\images\health_assitant_chatbot_icon.png"),
+        # Retrieve the run status
+        run_status = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
         )
 
-        with gr.Row():
-            txt = gr.Textbox(
-                scale=4,
-                show_label=False,
-                placeholder="Enter text and press enter, or upload an image",
-                container=False,
+        # If run is completed, get messages
+        if run_status.status == 'completed':
+            messages = client.beta.threads.messages.list(
+                thread_id=thread.id
             )
-            btn = gr.UploadButton("📁", file_types=["image", "video", "audio"])
+            response = ""
+            
+            data = messages.data
+            first_thread_message = data[0]
+            content = first_thread_message.content
+            response = content[0].text.value
+            return response
+        else:
+            continue
 
-        txt_msg = txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
-            bot, chatbot, chatbot, api_name="bot_response"
-        )
-        txt_msg.then(lambda: gr.Textbox(interactive=True), None, [txt], queue=False)
-        file_msg = btn.upload(add_file, [chatbot, btn], [chatbot], queue=False).then(
-            bot, chatbot, chatbot
-        )
+# Create a Gradio Interface
 
-        chatbot.like(print_like_dislike, None, None)
+iface = gr.ChatInterface(main, title="SAFe Specialist",\
+                          description="SAFe Specialist guiding transitions with realistic and \
+                            optimistic advice towards a product centric approach",\
+                                examples=["How can I shift from project to product mode?",\
+                                          "What are the key SAFe principles for my organization?",\
+                                            "Can you provide options for agile practices in my setting?",\
+                                                "How do I deal with cultural resistance in SAFe adoption?", \
+                                                    "What's your advice for an org with many different digital solutions?",\
+                                                        "Could you walk me through the step-by-step process of moving into SAFe?"]).queue()
 
-with gr.Tab("Medical Report storage"):
-    with gr.Row("Upload your document here:"):
-
-
-demo.queue()
 if __name__ == "__main__":
-    demo.launch(server_port=7860)
-
+    iface.launch()
--- utils/chatbot.py
+++ utils/chatbot.py
@@ -10,23 +10,21 @@ assistant = client.beta.assistants.create(
     name="meroHeatlhAI",
     instructions="You are a highly qualified and skilled doctor who can ask all the right questions to the patient and create an engaging and interesting conversation and make patients let out all the diseases they are suffering from. Then you will create a medical report based on the symptoms. If you are 100% sure, you can also predict the disease else just report the symptoms in a formal formatted diagnosis report. Make sure to include all the vital informations by asking the patients. Ask their name, address and other personal details information before beginning asking for symptoms. Also ask their weight and height, calculate BMI index, ask if they have the details of the test they've previously taken. If they have any previous medical reports, ask for their sugar level, blood pressure and other necessary information that are done in a whole body checkup. Ask one question at a time so that the user doesn't feel overwhelmed. After completing asking the symptoms, automatically generate the symptoms in a medical report like format along with the patient's information.",
     tools=[{"type": "code_interpreter"}],
-    model="gpt-4-turbo-preview"
+    model="gpt-3.5-turbo",
 )
 
-def chat_response():
-
+def chat_response(user_input,thread_id):
     thread = client.beta.threads.create()
-
+    user_input= user_input
     message = client.beta.threads.messages.create(
         thread_id=thread.id,
         role="user",
-        content="I am having panic attack. Can you help me?"
+        content=user_input
     )
 
     run = client.beta.threads.runs.create(
     thread_id=thread.id,
     assistant_id=assistant.id,
-    instructions="Please address the user as Jane Doe. The user has a premium account."
     )
 
     run = client.beta.threads.runs.retrieve(
@@ -37,13 +35,14 @@ def chat_response():
     messages = client.beta.threads.messages.list(
     thread_id=thread.id
     )
+    
+    chat_response_message = messages.data[0].content[0].text.value
+    return chat_response_message
 # Create the Gradio interface
 with gr.Blocks() as iface:
-    gr.Textbox(lines=3, label="Set the context and ask your question:")
     gr.ChatInterface(    
     fn=chat_response,
-    title="Chat with this bot!",
-    
+    title="Chat with this bot!"
 )
 
 # Launch the interface
